4167881c3ca020d19e14dda88c57d11c141a4b5f
piyushjadhavop <pj344685@gmail.com>
2026-05-24 20:29:10 +0530

    update and plot a graph

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
-
+import plotly.express as px
 # Load trained model
 model = joblib.load('model.pkl')
 
@@ -20,12 +20,6 @@ st.write("Enter player statistics to predict goals.")
 # Input Fields
 matches_played = st.number_input(
     "Matches Played",
-    min_value=0,
-    step=1
-)
-
-substitution = st.number_input(
-    "Substitution",
     min_value=0,
     step=1
 )
@@ -71,8 +65,8 @@ if st.button("Predict Goals"):
 
     # Create dataframe from user input
     input_data = pd.DataFrame({
+
         'Matches_Played': [matches_played],
-        'Substitution': [substitution],
         'Mins': [mins],
         'xG Per Avg Match': [xg_avg],
         'Shots': [shots],
@@ -88,3 +82,52 @@ if st.button("Predict Goals"):
     st.success(
         f"Predicted Goals: {prediction[0]:.2f}"
     )
+    # -------------------------------
+# BAR VISUALIZATION
+# -------------------------------
+
+
+feature_df = pd.DataFrame({
+    'Features': [
+        'Matches Played',
+        'Minutes',
+        'xG Per Avg Match',
+        'Shots',
+        'On Target',
+        'Shots Per Avg Match',
+        'On Target Per Avg Match',
+        'Predicted Goals'
+    ],
+    'Values': [
+        matches_played,
+        mins,
+        xg_avg,
+        shots,
+        on_target,
+        shots_avg,
+        on_target_avg,
+        prediction[0]
+    ]
+})
+
+bar_fig = px.bar(
+    feature_df,
+    x='Features',
+    y='Values',
+    text='Values',
+    color='Features',
+    title='Player Statistics & Predicted Goals'
+)
+
+# Improve Layout
+bar_fig.update_layout(
+    xaxis_title="Features",
+    yaxis_title="Values",
+    showlegend=False
+)
+
+# Display Graph
+st.plotly_chart(
+    bar_fig,
+    use_container_width=True
+)
